76ad85987f4b55a2b7ea80e8b2ffb238ffcb07c3
nssv-05 <nssv-05@SO-PC-116>
2014-06-02 15:29:10 +0900

    課題

---
+++ DataCollection/src/com/masa/datacollection/MainActivity.java
@@ -0,0 +1,227 @@
+package com.masa.datacollection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Xml;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private WebView varWebView;
+	private TextView mView;
+	static private String mArticleTitle[];
+	static private String mArticleURL[];
+	static private String mArticleOverview[];
+	static private int mArticleNum;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+
+		getArticle(createURL());
+		ListView list = (ListView)findViewById(R.id.ListView01);
+
+		ArrayList<ListItem> arrayList = new ArrayList<ListItem>();
+		for(int i=0;i< mArticleNum;i++){
+			arrayList.add(new ListItem(mArticleTitle[i],mArticleURL[i],mArticleOverview[i]));
+		}
+
+		//list.setAdapter(new ListArrayAdapter(this,arrayList));
+
+		//varWebView =(WebView)findViewById(R.id.webview);
+		varWebView.setWebViewClient(new CustomBrowserClient());
+		varWebView.getSettings().setJavaScriptEnabled(true);
+
+
+	}
+
+
+	public String createURL(){
+		String apiURL = "http://news.yahooapis.jp/NewsWebService/V2/topics?";
+		String appid = "dj0zaiZpPTU3VEFwYzNYYThSQSZzPWNvbnN1bWVyc2VjcmV0Jng9NWQ-";
+		String category ="top";
+		return String.format("%sappid=%s&pickupcategory=%s", apiURL, appid,category);
+	}
+
+
+	public static String httpGet(String strURL){
+		try{
+			URL url=new URL(strURL);
+			URLConnection connection = url.openConnection();
+			connection.setDoInput(true);
+			InputStream stream = connection.getInputStream();
+			readXML(stream);
+			String data="";
+			for(int i=0;i<mArticleNum;i++){
+				data+=mArticleTitle[i];
+			}
+			stream.close();
+			return data;
+
+		}catch(Exception e){
+			return e.toString();
+		}
+	}
+
+	public static void readXML(InputStream stream) throws XmlPullParserException{
+			try{
+				XmlPullParser myxmlPullParser = Xml.newPullParser();
+				myxmlPullParser.setInput(stream,"UTF-8");
+
+				int cntTitle = 0;
+				int cntAddress = 0;
+				int cntOverview = 0;
+				for(int e = myxmlPullParser.getEventType(); e!= XmlPullParser.END_DOCUMENT;
+						e = myxmlPullParser.next()){
+
+					if (e == XmlPullParser.START_TAG) {
+		                  if (myxmlPullParser.getName().equals("ResultSet")) {
+		                      mArticleNum = Integer.parseInt(myxmlPullParser.getAttributeValue(null, "totalResultsReturned"));
+		                      mArticleTitle = new String[mArticleNum];
+		                      mArticleURL = new String[mArticleNum];
+		                      mArticleOverview = new String[mArticleNum];
+		                  } else if (myxmlPullParser.getName().equals("Title")) {
+		                      mArticleTitle[cntTitle] = myxmlPullParser.nextText();
+		                      cntTitle++;
+		                  } else if (myxmlPullParser.getName().equals("Overview")) {
+		                      mArticleOverview[cntOverview] = myxmlPullParser.nextText();
+		                      cntOverview++;
+		                  } else if (myxmlPullParser.getName().equals("SmartphoneUrl")) {
+		                      mArticleURL[cntAddress] = myxmlPullParser.nextText();
+		                      cntAddress++;
+		                  }
+		               }
+		           }
+		     } catch (XmlPullParserException e) {
+		    	 e.printStackTrace();
+		     } catch (IOException e) {
+		    	 e.printStackTrace();
+		  }
+	}
+
+	public static void getArticle(String strURL){
+		try{
+			URL url=new URL(strURL);
+			URLConnection connection = url.openConnection();
+			connection.setDoInput(true);
+			InputStream stream = connection.getInputStream();
+			readXML(stream);
+			stream.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public class ListItem{
+		public String title;
+		public String url;
+		public String overview;
+
+		public ListItem(String title, String url, String overview){
+			this.title=title;
+			this.url=url;
+			this.overview=overview;
+		}
+	}
+	private class CustomBrowserClient extends WebViewClient{
+		public boolean shoudOverrideUrlLoading(WebView view,String url){
+			view.loadUrl(url);
+			return true;
+		}
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+
+		for(int i=0;i < mArticleNum ;i++){
+			menu.add(0,i,i,mArticleTitle[i]);
+		}
+		return true;
+	}
+	public boolean onOptionsItemSelected(MenuItem item){
+		super.onOptionsItemSelected(item);
+		int iid=item.getItemId();
+
+		for(int i=0;i < mArticleNum ;i++){
+			if(i==iid){
+				varWebView.loadUrl(mArticleURL[i]);
+			}
+		}
+		return true;
+	}
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		if ((keyCode==KeyEvent.KEYCODE_BACK)&&varWebView.canGoBack()){
+			varWebView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode,event);
+	}
+
+
+
+	public class ListArrayAdapter extends ArrayAdapter<ListItem> implements View.OnClickListener, OnInitListener {
+		private ArrayList<ListItem> listItem;
+		private TextToSpeech tts;
+		public ListArrayAdapter(Context context, ArrayList<ListItem> listItem){
+			super(context,R.layout.rowitem,listItem);
+			this.listItem=listItem;
+			tts = new TextToSpeech(getApplicationContext(), this);
+
+		}
+
+		@Override
+		public void onInit(int status){
+		}
+
+		@Override
+		public View getView(int positopn,View view, ViewGroup parent){
+			ListItem item = listItem.get(positopn);
+			Context context=getContext();
+			LinearLayout linearLayout=new LinearLayout(context);
+			view=linearLayout;
+			Button button=new Button(context);
+			button.setText("日本語");
+			button.setTag(String.valueOf(positopn));
+			button.setOnClickListener(this);
+			linearLayout.addView(button,0);
+			TextView textView= new TextView(context);
+			textView.setText(item.title);
+			linearLayout.addView(textView);
+
+			return view;
+		}
+
+
+		public void onClick(View view){
+			int tag = Integer.parseInt((String)view.getTag());
+			ListItem item = listItem.get(tag);
+			tts.speak(item.overview ,TextToSpeech.QUEUE_FLUSH,null);
+		}
+	}
+}
+
